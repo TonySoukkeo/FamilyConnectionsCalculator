1e70352bc34c63feedd092c7e8b174090190f353
Tony Soukkeo <TonySoukkeo@Tonys-MacBook-Pro.local>
2019-05-27 22:35:50 -0600

    Updates

---
+++ precache-manifest.f16f451846bf02815d70cc0daf952960.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0426e6c7d0150dff819d0c7bbccfe319",
+    "url": "/FamilyConnectionsCalculator/index.html"
+  },
+  {
+    "revision": "667be8fa53c1d1fbf0bc",
+    "url": "/FamilyConnectionsCalculator/static/css/main.bffaf617.chunk.css"
+  },
+  {
+    "revision": "3323d4c1cb605aa82517",
+    "url": "/FamilyConnectionsCalculator/static/js/2.1d01dada.chunk.js"
+  },
+  {
+    "revision": "667be8fa53c1d1fbf0bc",
+    "url": "/FamilyConnectionsCalculator/static/js/main.13d21a93.chunk.js"
+  },
+  {
+    "revision": "adbb055f8ee45093cfd0",
+    "url": "/FamilyConnectionsCalculator/static/js/runtime~main.c731e5de.js"
+  }
+]);
